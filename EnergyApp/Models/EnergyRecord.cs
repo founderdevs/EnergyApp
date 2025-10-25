799b06f54e651294b3c608b1bc2f22120cdb4c2a
founderdevs <Kurush333@yandex.ru>
2025-10-25 21:18:36 +0300

    Первые наброски

---
+++ EnergyApp/Models/EnergyRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyApp.Models
+{
+    public class EnergyRecord
+    {
+        public DateTime Date { get; set; } = DateTime.Now;
+        public double KilowattHours { get; set; }
+        public double CostPerKwh { get; set; }
+        public double TotalCost => KilowattHours * CostPerKwh;
+    }
+}
