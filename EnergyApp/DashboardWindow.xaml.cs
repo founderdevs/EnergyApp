c3c80f949401c041821f35ee21467e5441301c0b
founderdevs <Kurush333@yandex.ru>
2025-10-27 17:43:05 +0300

    Добавлен экран входа

---
+++ EnergyApp/DashboardWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EnergyApp
+{
+    public partial class DashboardWindow : Window
+    {
+        public DashboardWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EnergyApp/LoginWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyApp
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent(); // Обязательно — генерируется из XAML
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Простая проверка (заменишь на реальную позже)
+            if (login == "admin" && password == "1234")
+            {
+                var dashboard = new DashboardWindow();
+                dashboard.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка входа",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
